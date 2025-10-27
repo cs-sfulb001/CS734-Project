b76d5f0d6803cef6f09faac209fe6aa884645ce6
cs-sfulb001 <sfulb001@odu.edu>
2025-10-26 23:16:35 -0400

    Data set and some website modification

--- RAG/src/app/querry-results/querry-results.ts
+++ RAG/src/app/querry-results/querry-results.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './querry-results.css',
 })
 export class QuerryResults {
-
+  search(querry: string) {
+    }
 }
---
+++ RAG/src/app/result.ts
@@ -0,0 +1,6 @@
+export interface result {
+  id: number;
+  title: string;
+  summary: string;
+  content: string;
+}
